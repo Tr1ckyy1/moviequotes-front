11d1282238d1121f01928b19eb79d65732ecef25
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-05-13 15:01:19 +0400

    refactor: changed retrieving and updating user profile data

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
 <script setup lang="ts">
 import ToastMessage from '@/ui/ToastMessage.vue'
 import { useAuthStore } from './stores/AuthStore'
+import { getCsrfCookie } from './plugins/axios'
 const authStore = useAuthStore()
 
+getCsrfCookie()
 if (localStorage.getItem('loggedIn')) authStore.setUser(true)
 </script>
