b61e2f63691231b4fd3336d68e5e1dc033055262
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-05-27 13:01:50 +0400

    refactor: moved date formatter to helper

--- src/components/NotificationItem.vue
+++ src/components/NotificationItem.vue
@@ -34,10 +34,10 @@
           }}
         </p>
       </div>
-      <p class="lg:hidden">{{ formatRelativeDate }}</p>
+      <p class="lg:hidden">{{ formattedRelativeDate }}</p>
     </section>
     <section class="hidden lg:block ml-auto">
-      <p>{{ formatRelativeDate }}</p>
+      <p>{{ formattedRelativeDate }}</p>
       <p v-if="!notification.read_at" class="text-profile-green">{{ t('notifications.new') }}</p>
     </section>
   </li>
@@ -47,13 +47,13 @@
 import QuotesIcon from '@/components/icons/QuotesIcon.vue'
 import HeartIcon from '@/components/icons/HeartIcon.vue'
 import { useI18n } from 'vue-i18n'
-import { format } from 'date-fns'
 import { computed } from 'vue'
 import { markAsRead } from '@/services/api/notifications'
 
 import type { Notification } from '@/types'
 import { useNotificationsStore } from '@/stores/NotificationsStore'
 import { useQuotesStore } from '@/stores/QuotesStore'
+import formatDate from '@/helpers/formatDate'
 
 const props = defineProps<{
   notification: Notification
@@ -79,41 +79,43 @@ async function markNotification() {
   openViewQuoteModal(props.notification.quote_id)
 }
 
-const formatRelativeDate = computed(() => {
-  const now = new Date()
-  const createdDate = new Date(props.notification.created_at)
+const formattedRelativeDate = computed(() => formatDate(props.notification.created_at))
 
-  const diff = now.getTime() - createdDate.getTime()
+// const formatRelativeDate = computed(() => {
+//   const now = new Date()
+//   const createdDate = new Date(props.notification.created_at)
 
-  const secondsAgo = Math.floor(diff / 1000)
+//   const diff = now.getTime() - createdDate.getTime()
 
-  const intervals = [
-    { label: t('notifications.seconds_ago', { count: secondsAgo }), threshold: 60 }, // Less than 1 minute
-    {
-      label: t('notifications.minutes_ago', { count: Math.floor(secondsAgo / 60) }),
-      threshold: 3600
-    },
-    {
-      label: t('notifications.hours_ago', { count: Math.floor(secondsAgo / 3600) }),
-      threshold: 86400
-    },
-    {
-      label: t('notifications.days_ago', { count: Math.floor(secondsAgo / 86400) }),
-      threshold: 2592000
-    },
-    {
-      label: t('notifications.months_ago', { count: Math.floor(secondsAgo / 2592000) }),
-      threshold: 31536000
-    },
-    { label: format(createdDate, 'dd/MM/yyyy'), threshold: 63072000 } // More than 2 years (exact date)
-  ]
+//   const secondsAgo = Math.floor(diff / 1000)
 
-  const interval = intervals.find((interval) => secondsAgo < interval.threshold)
+//   const intervals = [
+//     { label: t('notifications.seconds_ago', { count: secondsAgo }), threshold: 60 }, // Less than 1 minute
+//     {
+//       label: t('notifications.minutes_ago', { count: Math.floor(secondsAgo / 60) }),
+//       threshold: 3600
+//     },
+//     {
+//       label: t('notifications.hours_ago', { count: Math.floor(secondsAgo / 3600) }),
+//       threshold: 86400
+//     },
+//     {
+//       label: t('notifications.days_ago', { count: Math.floor(secondsAgo / 86400) }),
+//       threshold: 2592000
+//     },
+//     {
+//       label: t('notifications.months_ago', { count: Math.floor(secondsAgo / 2592000) }),
+//       threshold: 31536000
+//     },
+//     { label: format(createdDate, 'dd/MM/yyyy'), threshold: 63072000 } // More than 2 years (exact date)
+//   ]
 
-  if (interval) {
-    return interval.label
-  } else {
-    return format(createdDate, 'dd/MM/yyyy')
-  }
-})
+//   const interval = intervals.find((interval) => secondsAgo < interval.threshold)
+
+//   if (interval) {
+//     return interval.label
+//   } else {
+//     return format(createdDate, 'dd/MM/yyyy')
+//   }
+// })
 </script>
