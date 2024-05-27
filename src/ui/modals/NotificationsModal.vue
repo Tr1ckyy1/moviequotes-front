6b392da077482c083080bc74e85f1c1e472b86d8
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-05-27 13:10:18 +0400

    chore: added env to gitignore

--- src/ui/modals/NotificationsModal.vue
+++ src/ui/modals/NotificationsModal.vue
@@ -3,22 +3,25 @@
     class="rounded-xl fixed h-screen top-16 lg:top-[90px] right-0 lg:right-14 w-full lg:w-1/2 lg:h-3/4 lg:max-h-[52rem] scrollbar overflow-y-auto bg-black text-white px-6 py-8"
     ref="container"
   >
-    <header class="flex items-center justify-between gap-2">
-      <h1 class="text-xl lg:text-3xl font-medium">{{ t('notifications.notifications') }}</h1>
-      <button @click="markAllNotifications" class="text-sm lg:text-xl border-b">
-        {{ t('notifications.mark_all') }}
-      </button>
-    </header>
-    <main class="mt-6">
-      <ul class="space-y-2">
-        <NotificationItem
-          v-for="notification in notificationsStore.notifications"
-          :key="notification.id"
-          :notification="notification"
-          @close-modal="closeModal"
-        />
-      </ul>
-    </main>
+    <div v-if="notificationsStore.notifications.length > 0">
+      <header class="flex items-center justify-between gap-2">
+        <h1 class="text-xl lg:text-3xl font-medium">{{ t('notifications.notifications') }}</h1>
+        <button @click="markAllNotifications" class="text-sm lg:text-xl border-b">
+          {{ t('notifications.mark_all') }}
+        </button>
+      </header>
+      <main class="mt-6">
+        <ul class="space-y-2">
+          <NotificationItem
+            v-for="notification in notificationsStore.notifications"
+            :key="notification.id"
+            :notification="notification"
+            @close-modal="closeModal"
+          />
+        </ul>
+      </main>
+    </div>
+    <p class="text-2xl lg:text-4xl" v-else>{{ t('notifications.no_notifications') }}</p>
   </div>
 </template>
 
