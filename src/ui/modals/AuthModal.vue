5ae73b0262089c11cdab0122a1183210222def70
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-06-05 16:06:02 +0400

    fix: fixed password field showing for gmail and toast minor issue

--- src/ui/modals/AuthModal.vue
+++ src/ui/modals/AuthModal.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="bg-gradient-to-r from-[#181623] via-dark-secondary to-[#0D0B14] p-6 px-5 space-y-6 w-full max-w-[40rem] h-full lg:h-auto lg:min-h-fit lg:max-h-[90%] lg:overflow-y-auto overflow-y-scroll scrollbar-none lg:from-modal-bg lg:via-modal-bg lg:to-modal-bg"
+    class="bg-gradient-to-r from-[#181623] via-dark-secondary to-[#0D0B14] p-5 space-y-6 w-full max-w-[40rem] h-full lg:h-auto lg:min-h-fit lg:max-h-[90%] lg:overflow-y-auto overflow-y-scroll scrollbar-none lg:from-modal-bg lg:via-modal-bg lg:to-modal-bg"
   >
     <CancelIcon @click="closeModal" class="ml-auto lg:cursor-pointer" />
     <div class="text-center space-y-4 lg:w-2/3 lg:mx-auto">
