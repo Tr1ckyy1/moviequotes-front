e7660b9b89e6a79a0a8ffba1e1d1173d0d34d840
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-06-07 18:35:02 +0400

    chore: adjusted width on landing quotes

--- src/components/home/HomeParallax.vue
+++ src/components/home/HomeParallax.vue
@@ -3,7 +3,7 @@
     class="w-full before:bg-[linear-gradient(to_bottom,black_0%,transparent_50%),linear-gradient(to_right,black_0%,transparent_50%)] before:w-full relative before:absolute before:-top-1 before:left-0 before:bottom-0"
   >
     <div
-      class="absolute top-1/2 -translate-y-1/2 lg:-translate-y-0 lg:top-1/3 left-12 lg:left-32 space-y-4 w-1/2"
+      class="absolute top-1/2 -translate-y-1/2 lg:-translate-y-0 lg:top-1/3 left-12 lg:left-32 space-y-4 w-[70%] lg:w-2/5"
     >
       <h1
         class="text-white font-bold lg:text-5xl before:lg:w-12 before:w-4 before:h-[2px] before:bg-white relative before:absolute before:right-full before:mr-4 before:top-8 text-xl leading-relaxed lg:leading-relaxed"
